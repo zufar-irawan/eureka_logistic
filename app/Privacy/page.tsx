8b89eee6349954d938d71dc32e645d5b5304ee39
FzeeyNa <zeerayzan@gmail.com>
2025-07-09 10:23:38 +0700

    added privasy page

---
+++ app/Privacy/page.tsx
@@ -0,0 +1,14 @@
+import Privacy from '@/components/Privacy'
+import Header from '@/components/header';
+import Footer from '@/components/Footer';
+import { div } from 'framer-motion/client';
+
+export default function PrivacyPage() {
+  return (
+    <div>
+    <Header/>
+    <Privacy/>
+    <Footer/>
+    </div>
+  );
+}
